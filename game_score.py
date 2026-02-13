105a15d79fa5414ce5020329629e04b00db4a231
Akhila26011142 <Akhila26011142@gmail.com>
2026-02-13 18:13:52 +0530

    Renamed file to correct assignment format

---
+++ game_score.py
@@ -0,0 +1,8 @@
+name = input("Enter the name: ")
+games_played = int(input("Enter the number of games played: "))
+total_Score = int(input("Enter the total score: "))
+average_score = total_Score / games_played
+print("\nPlayer:", name)
+print("Games Played:", games_played)
+print("Total Score:", total_Score)
+print("Average Score:", average_score)
